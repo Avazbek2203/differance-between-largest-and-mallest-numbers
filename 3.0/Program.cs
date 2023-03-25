60a55a467a70404630adf2f8640f37904ccf1e7f
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-03-25 15:05:29 +0500

    Add project files.

---
+++ 3.0/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = new int[4];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            bool validInput = false;
+
+            while (!validInput)
+            {
+                Console.Write($"Enter number {i + 1}: ");
+
+                double input;
+                if (double.TryParse(Console.ReadLine(), out input))
+                {
+                    numbers[i] = (int)input;
+                    validInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+            }
+        }
+
+        int smallest = numbers[0];
+        int largest = numbers[0];
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] < smallest)
+            {
+                smallest = numbers[i];
+            }
+            if (numbers[i] > largest)
+            {
+                largest = numbers[i];
+            }
+        }
+
+        int difference = largest - smallest;
+        Console.WriteLine($"The difference between the largest number {largest} and the smallest number {smallest} numbers is {difference}.");
+    }
+}
